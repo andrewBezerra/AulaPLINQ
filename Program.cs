0357f5317e9e6ec82d9a92202e23a874ab5351ef
Carlos Andrew Costa Bezerra <andrew@r2asistemas.com.br>
2020-06-22 23:54:06 -0300

    Adicionando arquivos do projeto.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AulaPLINQ
+{
+    class Program
+    {
+        private static void ReverseStringwithLinq()
+        {
+            string sentence = "the quick brown fox jumped over the lazy dog ";
+
+            var words = sentence.Split()
+                                .Select(word => new string(word.Reverse().ToArray()));
+
+            System.Console.WriteLine(string.Join(" ", words));
+        }
+        private static void ReverseStringwithPLinq()
+        {
+            string sentence = "the quick brown fox jumped over the lazy dog ";
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            var words = sentence.Split()
+                                .AsParallel() //PLINQ DECIDE QUANDO RODAR PARALELAMETE - ISSO É O PADRÃO.
+                                .AsOrdered()
+                                .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                                .Select(word => new string(word.Reverse().ToArray()));
+
+            System.Console.WriteLine(string.Join(" ", words));
+            st.Stop();
+            Console.WriteLine($"Tempo decorrido {st.Elapsed}");
+        }
+        static void Main(string[] args)
+        {
+            ReverseStringwithPLinq();
+        }
+    }
+}
